231e17371bac1869e021a70af929937f106aa7c3
Benedikt Heimel <benedikt.heimel@gmx.de>
2015-01-17 16:56:35 +0700

    first

--- sketch.js
+++ sketch.js
@@ -1,77 +1,95 @@
 $(document).ready(function(){
-//draw 4x4 GRID 
-$('div').append("<div id='container'></div>");
-for(var g = 0; g < 4;g++){
-for(var h = 0; h < 4; h++){
-$('#container').append("<div class='null'></div>");
-}
-}
-$('.null').width(""+(25-(200/960))+"%");
-$('.null').height(""+(25-(200/960))+"%");
-//Clear GRID
-$('#clear').click(function(){
-$('.simple').remove();
-$('.random').remove();
-$('.null').remove();
-$('.trail').remove();
+
+	//draw 4x4 GRID 
+	$('div').append("<div id='container'></div>");
+	for(var g = 0; g < 4; g++){
+		for(var h = 0; h < 4; h++){
+			$('#container').append("<div class='null'></div>");
+		}
+	}
+	$('.null').width("" + ( 25 - ( 200 / 960 )) + "%");
+	$('.null').height("" + ( 25 - ( 200 / 960 )) + "%");
+
+	//Clear GRID
+	$('#clear').click(function(){
+		$('.simple, .random, .null, .trail').remove();
+	});
+	
+	//Customize GRID
+	$('#random').click(function(){
+		$.fn.Grid('random');
+	});
+	
+	$('#simple').click(function(){
+		$.fn.Grid('simple');
+	});
+	
+	$('#trail').click(function(){
+		$.fn.Grid('trail');
+	});
 });
-//If Button clicked, customized GRID
-$('#random').click(function(){
-$.fn.Grid('random');
-});
-$('#simple').click(function(){
-$.fn.Grid('simple');
-});
-$('#trail').click(function(){
-$.fn.Grid('trail');
-});
-});
-//CHANGE COLOR SIMPLE
+
+
+//Colour change GRID
 $(document).on('mouseenter','.null',function(){
-$(this).css("background-color","green");
+	$(this).css("background-color","green");
 });
+
 $(document).on('mouseenter','.simple',function(){
-$(this).css("background-color","green");
+	$(this).css("background-color","green");
 });
-//CHANGE COLOR RANDOMLY
+
+//Random colour change GRID
 $(document).on('mouseenter','.random',function(){
-var colorRange = [];
-for(var c = 0; c < 3; c++){
-colorRange[c]= Math.floor(Math.random()*252);
-}
-$(this).css("background-color","rgb("+colorRange[0]+","+colorRange[1]+","+colorRange[2]+")");
+	
+	var colourCode = [];	
+	for(var c = 0; c < 3; c++){
+	colourCode[c]= Math.floor( Math.random() * 252 );
+	}
+	$(this).css( "background-color" , "rgb(" + colourCode[0] + "," + colourCode[1] + "," + colourCode[2] + ")" );
 });
+
 //CHANGE COLOR SIMPLE + leave trail
 $(document).on('mouseenter','.trail',function(){
-$(this).css("background-color","grey");});
-$(document).on('mouseleave','.trail',function(){
-var $di = $(this);
-setTimeout(function () {
-$di.css("background", "blue");
-}, 150);
+	$(this).css("background-color","grey");
 });
-//FUNCTION DEFINITION
-$.fn.Grid=function(id){
-$('.null, .simple, .random').remove();
-do{
-var ein = prompt("type in a number between 1 and 64");
-if(isNaN(ein)){
-alert("type in a number");
-}
-else if(ein< 1 || ein > 64){
-alert("Number not between 1 and 64");
-}
-else {break;}
-}
-while((isNaN(ein)) || (ein < 1 || ein > 64) )
-if(ein> 1 && ein < 64){
-var size = (100/ein)-(200/960);//include border
-for(var i = 0; i < ein; i++){
-for(var j = 0; j < ein; j++){
-$('#container').append("<div class='"+id+"'></div>");
-}
-}
-$("."+id+"").width(""+size+"%");
-$("."+id+"").height(""+size+"%");
-}
+$(document).on('mouseleave','.trail',function(){
+	var $currentDiv = $(this);
+	setTimeout(function () {
+	$currentDiv.css("background", "blue");
+	}, 
+	150);
+});
+
+//Customize GRID function definition
+$.fn.Grid=function(mode){
+	$('.null, .simple, .random').remove();
+
+	//Validation of input	
+	do{
+		var rowColNum = prompt("Type in the number of rows/columns (between 1 and 64)");
+
+		if(isNaN(rowColNum)){
+			alert("Type in a number");
+		}
+		else if(rowColNum < 1 || rowColNum > 64){
+			alert("Number not between 1 and 64");
+		}
+		else{
+			break;
+		}
+	}
+	while((isNaN(rowColNum)) || (rowColNum < 1 || rowColNum > 64) )
+
+	//put GRID into container
+	if(rowColNum> 1 && rowColNum < 64){
+		var size = (100 / rowColNum)-(200 / 960);//Include border
+		for(var i = 0; i < rowColNum; i++){
+			for(var j = 0; j < rowColNum; j++){
+				$('#container').append("<div class='" + mode + "'></div>");
+			}
+	}
+	$("." + mode + "").width("" + size + "%");
+	$("." + mode + "").height("" + size + "%");
+	}
 };
